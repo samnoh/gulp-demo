82954f8f29be2a041df1fcc40cb3d5962deda836
Sam Noh <samnoh21@outlook.com>
2019-07-16 22:15:31 +1200

    add - gulp-sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,11 @@ import gulp from 'gulp';
 import gulpPug from 'gulp-pug';
 import gulpWebserver from 'gulp-webserver';
 import gulpImage from 'gulp-image';
+import gulpSass from 'gulp-sass';
+import gulpAutoprefixer from 'gulp-autoprefixer';
+import gulpCsso from 'gulp-csso';
+
+gulpSass.compiler = require('node-sass');
 
 // Routes
 const routes = {
@@ -17,6 +22,11 @@ const routes = {
     img: {
         src: 'src/img/*',
         dest: 'build/img'
+    },
+    sass: {
+        watch: 'src/scss/**/*.scss',
+        src: 'src/scss/style.scss',
+        dest: 'build/css'
     }
 };
 
@@ -36,6 +46,20 @@ const img = () =>
         .pipe(gulpImage())
         .pipe(gulp.dest(routes.img.dest));
 
+const sass = () =>
+    gulp
+        .src(routes.sass.src)
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(gulpAutoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
+        .pipe(gulpCsso())
+        .pipe(gulp.dest(routes.sass.dest));
+
+const js = () =>
+    gulp
+        .src()
+        .pipe()
+        .pipe();
+
 // Live
 const webserver = () =>
     gulp.src('build').pipe(gulpWebserver({ livereload: true, open: true, port: 8000 }));
@@ -43,11 +67,12 @@ const webserver = () =>
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
+    gulp.watch(routes.sass.watch, sass);
 };
 
 // Run
 const prepare = gulp.series([clean, img]);
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, sass]);
 const live = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, live]);
