b7e1cdd88a8e25fb2e5e73da01263a0dafed6405
Sam Noh <samnoh21@outlook.com>
2019-07-16 12:24:56 +1200

    mod - gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,27 @@
 import gulp from 'gulp';
+import gulpPug from 'gulp-pug';
+import del from 'del';
+
+// Prepare
+const clean = () => del(['build/']);
+
+// Assets
+const routes = {
+    pug: {
+        src: 'src/*.pug',
+        dest: 'build'
+    }
+};
+
+const pug = () =>
+    gulp
+        .src(routes.pug.src)
+        .pipe(gulpPug())
+        .pipe(gulp.dest(routes.pug.dest));
+
+// Run
+const prepare = gulp.series([clean]);
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]);
+// export const build = gulp.series([]);
